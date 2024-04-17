47251a437ba6dd40ed00ab227787eb724b0165d7
faber222 <faberbernardo78@gmail.com>
2024-04-17 00:58:22 -0300

    :sparkeless: Formulando o byte

--- src/main.c
+++ src/main.c
@@ -9,11 +9,28 @@ void byteToBinary(unsigned char byte, char* binary) {
 	binary[8] = '\0';
 }
 
+unsigned char bitsToByte(char bits[]) {
+	unsigned char byte = 0;
+	int i;
+	for (i = 0; i < 8; i++) {
+		if (bits[i] == '1') {
+			byte |= (1 << (7 - i)); // Definir o bit correspondente em byte
+		} else if (bits[i] != '0') {
+			// Se o caractere não for '0' nem '1', indicando um erro
+			printf("Erro: O array de bits contém caracteres inválidos.\n");
+			return 0; // Ou retorne um valor que indique erro
+		}
+	}
+	return byte;
+}
+
 // recuperar dado da imagem
 void rread(bmp_img img) {
 	// Ler a imagem BMP recém-criada
 	bmp_img_read(&img, "triangulo.bmp");
-
+	int j = 0;
+	printf("\nrecovered data\n");
+	char bits[9];
 	// Exemplo de acesso aos pixels da imagem
 	for (size_t y = 0; y < 512; y++) {
 		for (size_t x = 0; x < 512; x++) {
@@ -22,22 +39,32 @@ void rread(bmp_img img) {
 			unsigned char b = img.img_pixels[y][x].blue;
 			// printf("Pixel (%zu,%zu): R=%u, G=%u, B=%u\n", y, x, r, g, b);
 			// Se o pixel for preto ou branco, imprimir seus dados
-			if ((r == 0 && g == 0 && b == 0)) {
-				printf("Pixel (%zu,%zu): 0\n", y, x);
-			} else if ((r == 250 && g == 250 && b == 250)) {
-				printf("Pixel (%zu,%zu): 1\n", y, x);
+			if ((r == 0 && g == 0 && b == 0) || (r == 250 && g == 250 && b == 250)) {
+				if (r == 0) {
+					bits[j] = '0';
+				} else {
+					bits[j] = '1';
+				}
+				j++;
+			}
+
+			if (j == 8) {
+				bits[j] = '\0'; // Adiciona terminador de string
+				printf("%s\n", bits);
+				j = 0;
 			}
 		}
 	}
+	printf("\n");
 }
 
-void wread(char byte, FILE* file, char binary[9], bmp_img img) {
+void wread(char byte, FILE* file, char* binary, bmp_img img) {
 	int y = 0;
 	int x = 0;
 	// Ler o arquivo byte a byte e imprimir sua representação binária
 	while (fread(&byte, sizeof(unsigned char), 1, file) == 1) {
 		byteToBinary(byte, binary);
-		printf("%s ", binary);
+		printf("%s \n", binary);
 		for (size_t i = 0; i < sizeof(binary); i++)
 		{
 			if (y == 510)
@@ -90,7 +117,7 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	
+
 	wread(byte, file, binary, img); // lê os dados de file em binario e converte para .bmp
 	fclose(file);
 	rread(img); // recupera os dados da imagem 
