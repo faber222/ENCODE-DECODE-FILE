22e35d52696e0683c2b730ddd811db3be05e5204
faber222 <faberbernardo78@gmail.com>
2024-04-23 00:49:54 -0300

    :bug: CORRIGIDO PROBLEMA, ERA O DEFINE ANTES DAS LIBS

--- src/cpp/function.h
+++ src/cpp/function.h
@@ -2,21 +2,22 @@
 
 #ifndef FILE_H
 #define FILE_H
-#define X 512 // largura da imagem
-#define Y 512 // altura da imagem
-#define N 9 // tamanho do byte
-#define UM 1 // valor usado no switch case para codificacao
-#define DOIS 2 // valor usado no switch case para decodificacao
-
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
 #include <cctype>
 #include <fstream>
+#include <opencv4/opencv2/videoio.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv4/opencv2/core.hpp>
 #include <filesystem>
-
 #include "./libs/libbmp.h"
+
+#define X 512 // largura da imagem
+#define Y 512 // altura da imagem
+#define N 9 // tamanho do byte
+#define UM 1 // valor usado no switch case para codificacao
+#define DOIS 2 // valor usado no switch case para decodificacao
 
 using namespace std;
 using namespace cv;
